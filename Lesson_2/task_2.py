6200ac4a09378ce5c0cef42536465eed059288a3
Lesh1y <luck_sky@list.ru>
2020-01-16 23:08:33 +0300

    Решение через рекурсию
    Добавлено решение через рекурсию

--- Lesson_2/task_2.py
+++ Lesson_2/task_2.py
@@ -19,19 +19,52 @@ def even_odd(num):
 
 
 print('Добро пожаловать в счётчик цифр \nУзнаем, сколько чётных и нечётных в вашем чмсле')
-while 1 > 0:
-    user_num = input('Введите 4-х значное натуральное число: ')
-    user_num = ' '.join(user_num)
+while True:
+    USER_NUM = input('Введите 4-х значное натуральное число: ')
+    USER_NUM = ' '.join(USER_NUM)
     try:
-        user_a, user_b, user_c, user_d = user_num.split(' ')
+        USER_A, USER_B, USER_C, USER_D = USER_NUM.split(' ')
         break
     except ValueError:
         print('Введено некорректное число. \nПовторите попытку.')
 
-even_odd(user_a)
-even_odd(user_b)
-even_odd(user_c)
-even_odd(user_d)
+even_odd(USER_A)
+even_odd(USER_B)
+even_odd(USER_C)
+even_odd(USER_D)
 print(f'{EVEN} чётных и {ODD} нечётных цифр в числе')
 
+
 # Вариант №2
+def calc_even(user_number):
+    """ функция рассчитывает количество четных цифр введённого числа"""
+
+    return len(str(user_number)) - calc_odd(user_number)
+
+
+def calc_odd(user_number):
+    """ функция вычисляет количество нечетных цифр введённого числа"""
+
+    if len(str(user_number)) == 1:
+        if (user_number % 2) == 0:
+            return 0
+        return 1
+    i = user_number // (10 ** (len(str(user_number)) - 1))
+    qty = 1 if (i % 2) else 0  # предварительное количество
+    return calc_odd(user_number - i *
+                    (10 ** (len(str(user_number)) - 1))) + qty
+
+
+while True:
+    try:
+        USER_NUMBER = int(input('Ведите натуральное число: '))
+        if USER_NUMBER <= 0:
+            print('Введите корректное натуральное число.')
+            continue
+        print(
+            f'Количество чётных цифр в числе {USER_NUMBER}: {calc_even(USER_NUMBER)}')
+        print(
+            f'Количество нечётных цифр в числе {USER_NUMBER}: {calc_odd(USER_NUMBER)}')
+        break
+    except ValueError:
+        print('Ведите натуральное число.')
